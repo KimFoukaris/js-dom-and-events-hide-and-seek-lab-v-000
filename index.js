99c01c90ddffe2a6bf967864ca3b8c96fc7de503
Kim Foukaris <kimfoukaris@outlook.com>
2018-07-03 17:09:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const ranks = document.querySelectorAll('.ranked-list li');
+
+  var i
+  for (i=0; i < ranks.length; ++i) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  let kids = document.querySelector('#grand-node div div div div');
+  return kids;
+}
